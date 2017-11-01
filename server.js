7166590f10449ca056a5b535e5f00271448dcc44
khoind <khoind@vng.com.vn>
2017-11-01 16:32:19 +0700

    update port for listen

--- server.js
+++ server.js
@@ -8,14 +8,12 @@ import config from './webpack.config';
 
 const compiler = webpack(config);
 
-const PORT = process.env.PORT || 3000;
-
 express()
   .use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
   .use(webpackHotMiddleware(compiler))
   .use('/assets', express.static(path.join(__dirname, 'assets')))
   .get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
-  .listen(PORT, 'localhost', (err) => {
+  .listen(process.env.PORT, 'localhost', (err) => {
     if (err) { console.log(err); }
     console.log(`Listening on port ${PORT}`);
   });
