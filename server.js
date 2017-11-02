a8e89c487f04ce4d1645d373ff23cb031b9bab8e
khoind <khoind@vng.com.vn>
2017-11-02 10:31:58 +0700

    update apple file

--- server.js
+++ server.js
@@ -8,11 +8,27 @@ import config from './webpack.config';
 
 const compiler = webpack(config);
 
+const appleData = 
+{
+  "applinks": {
+    "apps": [],
+    "details": [
+    {
+      "appID": "898EDDGZNP.vn.com.vng.zalopay.sb1",
+      "paths": ["*"]
+    }
+    ]
+  }
+};
 
 express()
   .use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
   .use(webpackHotMiddleware(compiler))
   .use('/assets', express.static(path.join(__dirname, 'assets')))
-  .use('/apple-app-site-association', express.static(path.join('apple-app-site-association')))
-  .get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
+  //.use('/apple-app-site-association', express.static(path.join('apple-app-site-association')))
+  //.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
+  .use('/apple-app-site-association', function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(appleData));
+  })
   .listen(process.env.PORT || 3000);            
