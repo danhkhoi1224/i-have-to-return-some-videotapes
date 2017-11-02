98402b95acd0c757baa5059555585c11f68888de
khoind <khoind@vng.com.vn>
2017-11-02 10:13:21 +0700

    add apple file

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ import config from './webpack.config';
 
 const compiler = webpack(config);
 
+
 express()
   .use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
   .use(webpackHotMiddleware(compiler))
   .use('/assets', express.static(path.join(__dirname, 'assets')))
+  .use('/apple-app-site-association', express.static(path.join('apple-app-site-association')))
   .get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
-  .listen(process.env.PORT);
-
-                          
+  .listen(process.env.PORT || 3000);            
